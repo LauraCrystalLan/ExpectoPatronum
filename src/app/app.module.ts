ad6e4b81a38582b77b31d746fb18d4d50d81b8a5
Lan <952694050@qq.com>
2023-01-20 15:46:57 +0800

    添加material组件库以及升级angular版本为15

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,11 @@ import { TafSnackBarComponent } from './components/taf-snack-bar/taf-snack-bar.c
 import { MainPageComponent } from './pages/main-page/main-page.component';
 import { PlatformModule } from '@angular/cdk/platform';
 import { MobileMainPageComponent } from './pages/mobile-main-page/mobile-main-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, TafSnackBarComponent, MainPageComponent, MobileMainPageComponent],
-  imports: [BrowserModule, AppRoutingModule, PlatformModule],
+  imports: [BrowserModule, AppRoutingModule, PlatformModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
